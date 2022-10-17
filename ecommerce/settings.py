f69da18aeca3b206ffa9342d66ea0529cd659b11
tamieclay <68555636+tamieclay@users.noreply.github.com>
2022-10-17 23:23:20 +0200

    Fourth commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-om^%dv7dgvlxq&^za^yo0#bor9)$i_@q4*!e%=cg1tvmc&0)ad
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
